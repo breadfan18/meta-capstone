49aabb8608a9b40289f3b1983dfeb3b1e72825d3
Swaroop Uprety <breadfan18@gmail.com>
2024-11-21 14:41:34 -0500

    setup main container

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,16 +1,25 @@
-import React from 'react'
+import React from "react";
 
 export default function Nav() {
   return (
     <nav>
-      <ul className='nav'>
-        <li><a href="/">Home</a></li>
-        <li><a href="/about">About</a></li>
-        <li><a href="/menu">Menu</a></li>
-        <li><a href="/reservations">Reservations</a></li>
-        <li><a href="/order-online">Order Online</a></li>
-        <li><a href="/login">Login</a></li>
+      <ul className="nav">
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/about">About</a>
+        </li>
+        <li>
+          <a href="/menu">Menu</a>
+        </li>
+        <li>
+          <a href="/reservations">Reservations</a>
+        </li>
+        <li>
+          <a href="/login">Login</a>
+        </li>
       </ul>
     </nav>
-  )
+  );
 }
