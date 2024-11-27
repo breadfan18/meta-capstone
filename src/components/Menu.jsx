c1c7efad881ccc4d22235cb5a9e0303974af2905
Swaroop Uprety <breadfan18@gmail.com>
2024-11-27 11:56:21 -0500

    before restyling menu

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -15,10 +15,12 @@ export default function Menu() {
           <img src="https://i.imgur.com/atPruMn.jpeg" alt="" />
           <section>
             <h3>Mediterranean Salad</h3>
-            <p>
-              Our Mediterranean salad is a mix of fresh greens, cherry tomatoes,
-              cucumbers, and feta cheese.
-            </p>
+            {!isMobile && (
+              <p>
+                Our Mediterranean salad is a mix of fresh greens, cherry
+                tomatoes, cucumbers, and feta cheese.
+              </p>
+            )}
             <FaCircleArrowRight className="menuGoIcon" />
           </section>
         </li>
@@ -26,10 +28,12 @@ export default function Menu() {
           <img src="https://i.imgur.com/JHDqyYG.jpeg" alt="" />
           <section>
             <h3>Meatball Sphagetti</h3>
-            <p>
-              Close your eyes and imagine the best meatball sphagetti you've
-              ever had in your life
-            </p>
+            {!isMobile && (
+              <p>
+                Close your eyes and imagine the best meatball sphagetti you've
+                ever had in your life
+              </p>
+            )}
             <FaCircleArrowRight className="menuGoIcon" />
           </section>
         </li>
@@ -37,10 +41,12 @@ export default function Menu() {
           <img src="https://i.imgur.com/GazI6zs.jpeg" alt="" />
           <section>
             <h3>Sushi Heaven</h3>
-            <p>
-              Got Sushi? We do! Come and let our sushi chefs treat you to the
-              best sushi you can find outside Japan
-            </p>
+            {!isMobile && (
+              <p>
+                Got Sushi? We do! Come and let our sushi chefs treat you to the
+                best sushi you can find outside Japan
+              </p>
+            )}
             <FaCircleArrowRight className="menuGoIcon" />
           </section>
         </li>
@@ -48,10 +54,12 @@ export default function Menu() {
           <img src="https://i.imgur.com/gU1NURi.jpeg" alt="" />
           <section>
             <h3>Brownie Decadance</h3>
-            <p>
-              Mix our decadent brownie with a scoop of delightful vanillca ice
-              cream and you'll be in heaven..
-            </p>
+            {!isMobile && (
+              <p>
+                Mix our decadent brownie with a scoop of delightful vanillca ice
+                cream and you'll be in heaven..
+              </p>
+            )}
             <FaCircleArrowRight className="menuGoIcon" />
           </section>
         </li>
