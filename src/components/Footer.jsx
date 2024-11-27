42bc1f4b37b470478aead0632757784cf735403e
Swaroop Uprety <breadfan18@gmail.com>
2024-11-27 13:29:53 -0500

    optimize footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,15 +11,12 @@ export default function Footer() {
   return (
     <>
       <footer className="footer">
-        <img
-          src="https://i.imgur.com/sExfMN4.png"
-          alt=""
-          style={{ width: "25rem" }}
-        />
-        <section>
-          <h4>Contact Us</h4>
-          <p>Phone Number</p>
-          <p>Email</p>
+        <section className="imgAndSocial">
+          <img
+            src="https://i.imgur.com/sExfMN4.png"
+            alt=""
+            style={{ width: "12rem" }}
+          />
           <article className="social-media-footer">
             <SlSocialGoogle />
             <SlSocialInstagram />
@@ -29,11 +26,15 @@ export default function Footer() {
           </article>
         </section>
         <section>
+          <h4>Contact Us</h4>
+          <p>Phone Number</p>
+          <p>Email</p>
+        </section>
+        <section>
           <h4>Our Locations</h4>
           <p>Silver Spring, MD</p>
           <p>San Francisco, CA</p>
           <p>Cincinnati, OH</p>
-          <p>New York, NY</p>
           <p>Chicago, IL</p>
         </section>
         <section>
